83ffa04ec496add8c5d605d3eb5abe76de5929e8
Henry Qi <henryqi222@gmail.com>
2016-10-23 18:30:53 -0600

    added necessary files and some bootstrapping

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('quizApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider){
+	
+	$urlRouterProvider
+		.otherwise('/');
+	
+	$stateProvider
+		.state('home', {
+			
+		
+		})
+	
+	
+	
+	
+	
+	
+	
+	
+})
